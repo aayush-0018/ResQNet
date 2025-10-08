94c5c74d28a6f1c7220877d4a88f6b35f9fc02b8
aayush-0018 <goswamiaayush8@gmail.com>
2025-10-09 01:16:25 +0530

    Changes for frontend script files

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // 👈 ensures it's accessible on Render or Docker
+    port: 5173,      // default for local dev
+  },
+  preview: {
+    host: '0.0.0.0', // 👈 ensures vite preview works on Render
+    port: 10000,     // matches your Render start command
+  },
 })
